803df76770d6d99f25a78113052a3a29de09f01c
Brandon Nuñez <inquiry@brandonstech.com>
2026-08-06 13:09:51 -0400

    AcI Python Automation Work

--- Python/requests_v2.py
+++ Python/requests_v2.py
@@ -1,6 +1,6 @@
 import requests
 import json
-#import xmltodict
+import yaml
 
 URL = "https://172.16.200.220/api/aaaLogin.json"
 BASE_URL = "https://172.16.200.220/api/mo/"
@@ -24,17 +24,26 @@ auth = auth()
 #print(auth)
 token = auth["imdata"][0]["aaaLogin"]["attributes"]["token"]
 cookie = {"APIC-cookie": token}
+print (cookie)
 
 def get_tenants():
     result = {}
     with requests.Session() as R:
         try:
-            response = R.get(f"{BASE_URL}uni/tn-common.json", cookies=cookie)
+            response = R.get(f"{BASE_URL}uni/tn-common.json", cookies=cookie, verify=False)
             response.raise_for_status
             result = response.json()
         except requests.exceptions.ConnectTimeout:
             print("Request Timed Out")
     return result
 
-tenant_data = get_tenants()
-print(json.dumps(tenant_data, indent=2))
+json_data = get_tenants()
+print(json_data)
+yaml_data = yaml.dump(json_data)
+print(yaml_data)
+#print(json.dumps(json_data, indent=2))
+
+with open(r"C:\Cool\GitHub\NetworkAutomation\Python\stuf.yml", "r") as yml:
+    python_yaml = yaml.safe_load(yml)
+    print("Brandon's Age is",python_yaml['Brandon']["Age"])
+    print(python_yaml)
